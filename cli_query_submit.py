9094f72a3e2095c5fdc7c5f12969e622f25ec1f7
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-02-10 14:19:49 +0000

    Retrieves the right set of documents.

--- cli_query_submit.py
+++ cli_query_submit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import itertools
 import sys
 import logging
 import core
@@ -10,9 +11,11 @@ from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import *
 
 from backend.db import UserQuery, UserQueryKeywordRecord, UserQueryDomainRecord, UserQueryArticleRecord
-from backend.db import Keyword, Domain
+from backend.db import Keyword, Domain, KeywordAdjacency, Article
 
 if __name__ == "__main__":
+
+    core.configure_logging()
 
     #
     # Argument processing
@@ -23,7 +26,7 @@ if __name__ == "__main__":
     # Database connection
     engine = core.get_database_engine_string()
     logging.info("Using connection string '%s'" % (engine,))
-    engine = create_engine(engine, encoding='utf-8', isolation_level="READ COMMITTED")
+    engine = create_engine(engine, encoding='utf-8')
     logging.info("Binding session...")
     session = Session(bind=engine, autocommit = False)
 
@@ -35,23 +38,68 @@ if __name__ == "__main__":
         q = it.one()
     except NoResultFound:
         q = UserQuery(query_text)
-
-    session.add(q)
-    session.commit()
+        session.add(q)
+        session.commit()
     assert q.id is not None 
-
+    logging.info("Resolving query id %d with text '%s'", q.id, q.text)
     #
     # Query keyword resolution
     keywords = set([])
+    using_keywords = False 
+    raw_keyword_count = 0
     for k in q.get_keywords(q.text):
+        logging.debug("keyword: %s", k)
+        using_keywords = True 
+        raw_keyword_count += 1
         it = session.query(Keyword).filter(Keyword.word.like("%s" % (k,)))
         keywords.update(it)
+    logging.info("Query(%d): expanded %d raw keywords into %d database terms", q.id, raw_keyword_count, len(keywords))
 
     #
     # Query domain resolution
     domains = set([])
+    using_domains = False 
+    raw_domain_count = 0
     for k in q.get_domains(q.text):
+        logging.debug("domain: %s", k)
+        raw_domain_count += 1
+        using_domains = True 
         it = session.query(Domain).filter(Domain.key.like("%%%s" % (k,)))
         domains.update(it)
+    logging.info("Query(%d): expanded %d raw domains into %d database domains", q.id, raw_domain_count, len(domains))
 
-    print domains
+
+    #
+    # Article domain resolution
+    documents_domains = set([])
+    for d in domains:
+        articles_it = session.query(Article).filter_by(domain = d)
+        for article in articles_it:
+            documents_domains.update(article.documents)
+    logging.info("Query(%d): retrieved %d documents relevant to a domain", q.id, len(documents_domains))
+    #
+    # Identify documents containing the given keywords 
+    documents_keywords = set([])
+    if raw_keyword_count > 1:
+        for id1, id2 in itertools.combinations(keywords, 2):
+            it = session.query(KeywordAdjacency).filter_by(key1 = id1).filter_by(key2 = id2)
+            documents_keywords.update([i.document for i in it])
+    else:
+        for keyword in keywords:
+            it = session.query(KeywordAdjacency).filter((KeywordAdjacency.key1 == keyword) | (KeywordAdjacency.key2 == keyword))
+            documents_keywords.update([i.document for i in it])
+    logging.info("Query(%d): retrieved %d documents relevant to a keyword", q.id, len(documents_keywords))
+
+    #
+    # Final article set resolution 
+    assert using_domains or using_keywords
+    documents = set([])
+    if using_domains and using_keywords:
+        documents = documents_keywords & documents_domains
+    elif using_domains:
+        documents = documents_domains 
+    elif using_keywords:
+        documents = documents_keywords
+    logging.info("Query(%d): final document set contains %d elements", q.id, len(documents))
+
+    print documents 
