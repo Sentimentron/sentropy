2ec4b6a9bc4ff2b9b7658a498f83e962eba54a36
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-02-19 13:32:58 +0000

    May be faster to use SQL, we'll see.

--- cli_caching.py
+++ cli_caching.py
@@ -22,24 +22,30 @@ def get_redis_instance():
     return redis.StrictRedis(host=host, port=6379, db=0)
 
 def cache_keywords():
+    core.configure_logging('debug')
     from backend.db import Keyword
     engine = core.get_database_engine_string()
     logging.info("Using connection string '%s'" % (engine,))
     engine = create_engine(engine, encoding='utf-8', isolation_level="READ UNCOMMITTED")
     session = Session(bind=engine, autocommit = False)
 
+    # Estimate the number of keywords
+    logging.debug("Estimating number of keywords...")
+    for count, in session.execute("SELECT COUNT(*) FROM keywords"):
+        total = count 
+
+    logging.debug("Establishing connection to redis...")
     r = get_redis_instance()
 
-    it = session.query(Keyword)
-    total = it.count()
     logging.info("Caching %d keywords...", total)
-
     cached = 0
-    for k in it:
+    for _id, word in session.execute("SELECT id, word FROM keywords"):
         r.set(k.word, k.id)
         cached += 1
-        if cached % 50 == 0:
+        if cached % 1000 == 0:
             logging.info("Cached %d keywords (%.2f%% done)", it.count(), 100*cached)
+
+    logging.info("Cached %d keywords (%.2f%% done)", it.count(), 100*cached)
 
 if __name__ == "__main__":
 
